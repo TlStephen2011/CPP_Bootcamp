5cf5b441a387a093b4ad5b87357e6eb416e20c5d
Tyrone Stephen <tlstephen11@gmail.com>
2019-06-04 13:48:48 +0200

    Completed error handling for files in ex07.

--- day01/ex07/main.cpp
+++ day01/ex07/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tstephen <tstephen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 12:27:10 by tstephen          #+#    #+#             */
-/*   Updated: 2019/06/03 16:14:18 by tstephen         ###   ########.fr       */
+/*   Updated: 2019/06/04 13:47:16 by tstephen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,12 @@ void searchAndReplace(std::string& str,
 
 static std::string getFileContent(const std::string& path)
 {
-    //TODO: Add error handling
-    std::ifstream file(path);
+    std::ifstream file(path, std::ios_base::in);
+    if (!file || file.fail())
+    {
+        throw std::ios::failure( "Error opening input file! File: " + path);
+    }
+    
     std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 
     return content;
@@ -48,8 +52,14 @@ static bool validateArguments(char *argv[])
 
 static void writeToFile(std::string filename, std::string content)
 {
-    //TODO: Add error handling
-    std::ofstream ofs(filename + ".replace", std::ios::out);
+    
+    std::ofstream ofs(filename + ".replace", std::ios_base::out);
+
+    if (!ofs || ofs.fail())
+    {
+        throw std::ios::failure( "Error opening file! File: " + filename);
+    }
+
     ofs << content;
     return;
 }
@@ -63,10 +73,29 @@ int main(int argc, char **argv)
     }
 
     if (validateArguments(argv) == true)
-    {        
-        std::string content = getFileContent(argv[1]);
+    {   
+        std::string content;
+        try
+        {
+            content = getFileContent(argv[1]);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << std::endl;
+            return 0;
+        }
+
         searchAndReplace(content, argv[2], argv[3]);
-        writeToFile(argv[1], content);
+
+        try
+        {
+            writeToFile(argv[1], content);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << std::endl;
+            return 0;
+        }
     }
     else
     {
